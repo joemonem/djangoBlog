6b5827c118195a78d0d020e19482e91a4f4ab9ef
Joe Monem <jmonem@icloud.com>
2023-11-17 11:02:06 +0200

    user authentication

--- joesblog/theblog/views.py
+++ joesblog/theblog/views.py
@@ -18,6 +18,7 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = "home.html"
+    # ordering = ["-published_date"]
 
 
 class ArticleDetailsView(DetailView):
