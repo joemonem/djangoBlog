0013134f55415d9536360902be2901618e2bedf7
Joe Monem <jmonem@icloud.com>
2023-11-20 07:56:49 +0200

    127.0.0.1 allowed_hosts

--- joesblog/joesblog/settings.py
+++ joesblog/joesblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-w61i@qa)f435zbel3cw@b@jo$23xjyws57tu26-&7(b)12e&wn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app"]
+ALLOWED_HOSTS = [".vercel.app", "127.0.0.1"]
 
 
 # Application definition
