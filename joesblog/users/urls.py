9daf2f7ae0782fa50ae3494dbec9722a4eba0839
Joe Monem <jmonem@icloud.com>
2023-11-22 15:51:05 +0200

    edit profile page, only shows when signed in

--- joesblog/users/urls.py
+++ joesblog/users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import UserRegistrationView
+from .views import UserRegistrationView, UserEditView
 
-urlpatterns = [path("signup/", UserRegistrationView.as_view(), name="signup")]
+urlpatterns = [
+    path("signup/", UserRegistrationView.as_view(), name="signup"),
+    path("edit_profile/", UserEditView.as_view(), name="edit_profile"),
+]
