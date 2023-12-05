ee435b9a2d9e4e3c07a8a3f0ca7f16b23c176d82
Joe Monem <jmonem@icloud.com>
2023-12-05 14:36:59 +0200

    handle case when user has no posts and no bio

--- joesblog/theblog/views.py
+++ joesblog/theblog/views.py
@@ -25,7 +25,7 @@ class HomeView(ListView):
     model = Post
     template_name = "home.html"
     context_object_name = "posts"
-    ordering = ["-published_date"]
+    # ordering = ["-published_date"]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
